487775b7df63e219a31f9c194c32e46bb7d8e919
duvan1026 <duvancastro1026@gmail.com>
2023-03-27 16:30:01 -0500

    update files and implementation the class GestorFacturas en Spring framwork

--- src/main/java/com/example/GestorFacturas.java
+++ src/main/java/com/example/GestorFacturas.java
@@ -4,12 +4,15 @@ public class GestorFacturas {
 
     // 1. atributos
     Calculadora calculadora;
+    String nombre;
 
 
     // 2. constructores
-    public GestorFacturas(Calculadora calculadora){
+    public GestorFacturas(Calculadora calculadora, String nombre){
 
+        System.out.println("Ejecutando constructor GestorFacturas");
         this.calculadora = calculadora;
+        this.nombre = nombre;
 
     }
 
