b12860efacbb21700cc8d026cb9f695f03a5bfc3
duvan1026 <duvancastro1026@gmail.com>
2023-03-27 16:07:25 -0500

    opction for object created in Spring

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,10 +8,19 @@ public class Main {
     public static void main(String[] args){
 
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        // Opción 1. Crear un objeto de forna normal
+        CalculatorServices service = new CalculatorServices();
+
+        // Opcion 2. Recibir  un objeto de Spring
         CalculatorServices calculadora = (CalculatorServices) context.getBean("calculatorService");
+
 
         String texto = calculadora.holaMundo();
         System.out.println(texto);
+
+
+
 
     }
 }
